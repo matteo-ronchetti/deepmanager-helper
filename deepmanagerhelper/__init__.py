1efa83127c0f4113381dddfc780b194cff12cb44
Matteo Ronchetti <mttronchetti@gmail.com>
2018-05-02 09:43:35 +0200

    real code

--- deepmanagerhelper/__init__.py
+++ deepmanagerhelper/__init__.py
@@ -1,2 +1,23 @@
-def sum(a, b):
-    return a + b
+import os
+import json
+
+
+class DeepManagerHelper:
+    @staticmethod
+    def input(path):
+        return os.path.join("input", path)
+
+    @staticmethod
+    def output(path):
+        return os.path.join("output", path)
+
+    @staticmethod
+    def _to_json(obj):
+        try:
+            return json.dumps(obj)
+        except:
+            return "{}"
+
+    @staticmethod
+    def log(**kwargs):
+        print("@deepmanager", DeepManagerHelper._to_json(kwargs), flush=True)
--- tests/base_test.py
+++ tests/base_test.py
@@ -1,13 +1,20 @@
 import sys
 import os
 import unittest
+import json
 
 # pylint: disable=wrong-import-order,wrong-import-position
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-import deepmanagerhelper as dmh
+from deepmanagerhelper import DeepManagerHelper
 
 
 class BaseTest(unittest.TestCase):
-    def test_nothing(self):
-        self.assertEqual(2, dmh.sum(1, 1))
+    def test_io(self):
+        self.assertEqual("input/file", DeepManagerHelper.input("file"))
+        self.assertEqual("output/file", DeepManagerHelper.output("file"))
+
+    def test_logging(self):
+        obj = {"accuracy": 0.3234}
+        self.assertEqual(json.dumps(obj), DeepManagerHelper._to_json(obj))
+        self.assertEqual("{}", DeepManagerHelper._to_json(self))
